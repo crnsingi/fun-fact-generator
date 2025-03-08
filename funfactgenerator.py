32cdf250969016ae77bf32d7b8e4b3d5a2967b27
Cesar <cesarnsingi@hotmail.com>
2025-03-08 20:23:38 +0200

    def function

--- funfactgenerator.py
+++ funfactgenerator.py
@@ -3,3 +3,14 @@ import requests
 from pywebio.input import *
 from pywebio.output import *
 from pywebio.session import *
+
+def get_fun_fact(_):
+    # Clear the screen
+    clear()
+    
+    #HTML content for the fun fact generator header
+    put_html(
+        '<p align="left">'
+        '<h2><img src="https://media.geeksforgeeks.org/wp-content/uploads/20210720224119/MessagingHappyicon.png" width="7%"> Fun Fact Generator</h2>'
+        '</p>'
+    )
